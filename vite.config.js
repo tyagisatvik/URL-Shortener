89d959814b357d65bacc26318f65a5eeb3bf290b
tyagisatvik <satviktyagi3@gmail.com>
2024-07-12 15:14:18 +0530

    project files updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+/* eslint-disable no-undef */
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
+  rules: {
+    "react/prop-types": 0,
+  },
+});
